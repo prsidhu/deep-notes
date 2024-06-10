784a0a97d465242681fa5a67bb694461f099d421
Preet Sidhu <preetsidhu.bits@gmail.com>
2024-06-10 13:42:07 -0400

    working functionality and updated readme

--- main.ts
+++ main.ts
@@ -1,14 +1,22 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import {
+	Editor,
+	MarkdownView,
+	Menu,
+	MenuItem,
+	Notice,
+	Plugin,
+	TAbstractFile,
+	TFile,
+	WorkspaceLeaf,
+} from "obsidian";
 
 interface DeepNotesSettings {
 	mySetting: string;
 }
 
 const DEFAULT_SETTINGS: DeepNotesSettings = {
-	mySetting: 'default'
-}
+	mySetting: "default",
+};
 
 export default class DeepNotes extends Plugin {
 	settings: DeepNotesSettings;
@@ -16,119 +24,76 @@ export default class DeepNotes extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('Hello, world!!!!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: "create-note-from-selection",
+			name: "Create New Note from Selection",
+			editorCallback: (editor: Editor, view: MarkdownView) =>
+				this.createNoteFromSelection(editor, view),
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		this.registerEvent(
+			this.app.workspace.on(
+				"editor-menu",
+				(menu: Menu, editor: Editor, view: MarkdownView) => {
+					menu.addItem((item: MenuItem) => {
+						item.setTitle("Create new note")
+							.setIcon("document")
+							.onClick(() =>
+								this.createNoteFromSelection(editor, view)
+							);
+					});
 				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			)
+		);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+	async createNoteFromSelection(editor: Editor, view: MarkdownView) {
+		const selectedText: string = editor.getSelection();
+		if (!selectedText) return;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+		// Replace or remove invalid characters for the filename, but keep spaces
+		const sanitizedFileName: string = selectedText
+			.trim()
+			.replace(/[\\/:*?"<>|#%&{}[\]@!$'+=]/g, " ");
+		const newNoteName: string = selectedText.trim(); // Keep spaces for the title
+		const newNotePath: string = sanitizedFileName + ".md";
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+		const newNoteContent = `# ${newNoteName}`;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: DeepNotes;
+		try {
+			// Create the new note
+			await this.app.vault.create(newNotePath, newNoteContent);
 
-	constructor(app: App, plugin: DeepNotes) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+			// Replace selected text with link to the new note
+			const linkToNewNote = `[[${sanitizedFileName}]]`;
+			editor.replaceSelection(linkToNewNote);
 
-	display(): void {
-		const {containerEl} = this;
+			// Notify user about the new note creation
+			new Notice(`New note created: ${newNoteName}`);
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			// Open the new note in a new tab (leaf)
+			const newFile: TAbstractFile | null =
+				this.app.vault.getAbstractFileByPath(newNotePath);
+			if (newFile && newFile instanceof TFile) {
+				const newLeaf: WorkspaceLeaf = this.app.workspace.getLeaf(true);
+				newLeaf.openFile(newFile);
+			}
+		} catch (error) {
+			new Notice(`Error creating note: ${error.message}`);
+		}
 	}
 }
